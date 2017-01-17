d796ef9c60076174b8c4018601490081b7882572
kevin plot <kevin.plot1@gmail.com>
2017-01-17 17:00:14 +0000

    add mailing

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,19 +7,19 @@ class ChargesController < ApplicationController
     def create
       # Amount in cents
       @reservation = Reservation.find(params[:reservation_id])
-    
       customer = Stripe::Customer.create(
         :email => params[:stripeEmail],
         :source  => params[:stripeToken]
       )
-    
+      
       charge = Stripe::Charge.create(
         :customer    => customer.id,
         :amount      => @reservation.totalprice_cents,
         :description => 'Rails Stripe customer',
         :currency    => 'usd'
+        
       )
-    
+    ReservationMailer.welcome_email(current_user, @reservation).deliver_later
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_charge_path

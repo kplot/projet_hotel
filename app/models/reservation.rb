8d8b2418fc4547181c68e8cb6a588e1f98db365e
noham <nohamboumakel@gmail.com>
2017-01-18 09:21:44 +0000

    Book conditions ok

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,14 +3,15 @@ class Reservation < ActiveRecord::Base
     belongs_to :room
     validates :leaved_at, :arrived_at, :room, presence: true
     validate :leaved_at_cannot_be_before_arrived_at
+    validate :already_booked
 
     
-    def nbnuit
+    def nb_night
         (leaved_at - arrived_at).to_i
     end
     
     def totalprice
-        nbnuit.to_i * room.price
+        nb_night* room.price
     end
     
     def totalprice_cents
@@ -22,13 +23,16 @@ class Reservation < ActiveRecord::Base
             errors.add(:leaved_at, "can't be before or equal arrived at")
         end
     end
-     
-   
-
-
     
-
-    
-    
-    
+    def already_booked
+        if leaved_at && arrived_at
+            (arrived_at..(leaved_at - 1.days)).each do |r|
+                if Reservation.where(room_id: room_id).where("reservations.arrived_at <= ? AND reservations.leaved_at >= ?", r, r + 1.days).count > 0
+                    errors.add(:room, "already booked")
+                    return
+                end
+            end
+        end
+    end
 end
+
